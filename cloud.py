c691dc5bb2c60be8d796268c823e2d1606375803
literature <65801567+LiteraturePro@users.noreply.github.com>
2022-09-11 18:03:15 +0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -5,54 +5,52 @@ import requests
 from bs4 import BeautifulSoup
 engine = Engine()
 
-# 内容爬取
-def get_info():
-    url = "http://www.wufazhuce.com/"
-    page = requests.get(url).content
-    from bs4 import BeautifulSoup
-    soup = BeautifulSoup(page, 'html.parser')
-    for i in soup.find_all('div',class_ = 'item'):
-        onelist = i.find_all('a')
-        image = onelist[0].img['src']
-        word = onelist[1].text
-        infolist = i.find_all('p')
-        id = infolist[0].text
-        date = infolist[1].text+' '+infolist[2].text
-    list = []
-    soup = BeautifulSoup(page, 'html.parser')
-    for i in soup.find_all('div',class_ = 'item'):
-        onelist = i.find_all('a')
-        image = onelist[0].img['src']
-        word = onelist[1].text
-        infolist = i.find_all('p')
-        id = infolist[0].text
-        date = infolist[1].text+' '+infolist[2].text
-        data = {
-            'image':image,
-            'word':word,
-            'id':id,
-            'date':date
-            }
-        list.append(data)
-    return list[0]
 
 @engine.define
 def get_infos(**params):
     if 'name' in params:
-        info = get_info().get("word")
+
+        return ''
+    
+    
+    else:
+        url = "http://www.wufazhuce.com/"
+        page = requests.get(url).content
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(page, 'html.parser')
+        for i in soup.find_all('div',class_ = 'item'):
+            onelist = i.find_all('a')
+            image = onelist[0].img['src']
+            word = onelist[1].text
+            infolist = i.find_all('p')
+            id = infolist[0].text
+            date = infolist[1].text+' '+infolist[2].text
+        list = []
+        soup = BeautifulSoup(page, 'html.parser')
+        for i in soup.find_all('div',class_ = 'item'):
+            onelist = i.find_all('a')
+            image = onelist[0].img['src']
+            word = onelist[1].text
+            infolist = i.find_all('p')
+            id = infolist[0].text
+            date = infolist[1].text+' '+infolist[2].text
+            data = {
+                'image':image,
+                'word':word,
+                'id':id,
+                'date':date
+                }
+            list.append(data)
         # 声明 class
         Words = leancloud.Object.extend('words')
 
         # 构建对象
         words = Words()
+        print(list[0])
 
         # 为属性赋值
-        words.set('word',   info)
+        words.set('word',   list[0].get("word"))
         # 将对象保存到云端
         words.save()
-        
-        return 'info'
-    
-    
-    else:
-        return 'Hello, LeanCloud!'
+
+        return list[0].get("word")
