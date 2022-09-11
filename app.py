af10d7bcc1bf31f4fb1510d89829b318187b34a2
literature <65801567+LiteraturePro@users.noreply.github.com>
2022-09-11 15:41:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask import Flask, jsonify, request
+from flask import render_template
+
+
+
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "hi"
+    return render_template('index.html')
+
+
+@app.route('/api')
+def time():
+    return str(datetime.now())
